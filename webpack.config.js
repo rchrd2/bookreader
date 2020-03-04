1f8f104bab7afd3cccd3af8347a7049457cc5270
Isa Herico-Velasco <isaherico@gmail.com>
2020-03-04 10:57:05 -0800

    add to webpack, .gitattributes

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = buildJSFiles([
     'plugins/plugin.archive_analytics.js',
     'plugins/plugin.autoplay.js',
     'plugins/plugin.iframe.js',
+    'plugins/plugin.mobile_nav.js',
     'plugins/tts/plugin.tts.js',
 ]);
 
